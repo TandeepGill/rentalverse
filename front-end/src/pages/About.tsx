07de958a7817de81ba2c4f5d795718bf0b7d72d2
Tandeep Gill <gill.tandeep@gmail.com>
2024-03-03 00:01:21 +0000

    Added react router with temporary routes.

---
+++ front-end/src/pages/About.tsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-orange-600'>About</h1>
+    </>
+  );
+}
+
+export default About;
---
+++ front-end/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-orange-600'>Home</h1>
+    </>
+  );
+}
+
+export default Home;
