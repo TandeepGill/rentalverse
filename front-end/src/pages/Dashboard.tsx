42893b18644eabee60132be7de60d1bbc20013ce
Tandeep Gill <gill.tandeep@gmail.com>
2024-03-27 20:27:23 +0000

    Added logic for header items changes when user is logged in.

--- front-end/src/pages/Dashboard.tsx
+++ front-end/src/pages/Dashboard.tsx
@@ -1,26 +1,7 @@
-import { useNavigate } from "react-router-dom";
-import { useAppDispatch } from "../app/hooks";
-import { logout } from "../features/authSlice";
-
 const Dashboard = () => {
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-
-  const logoutHandler = () => {
-    dispatch(logout());
-    navigate("/");
-  };
-
   return (
     <div className='flex h-screen items-center justify-center text-orange-600'>
       <h1 className='text-6xl'>Dashboard</h1>
-      <button
-        type='button'
-        className='flex justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600'
-        onClick={() => logoutHandler()}
-      >
-        Logout
-      </button>
     </div>
   );
 };
